46283ca86d137a6d7233395833e9ff4f85e3a929
Goferov <marcingodfryd@gmail.com>
2023-07-19 20:13:35 +0200

    modify debug.php and add templates

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>test</title>
-</head>
-<body>
-Hello world!
-</body>
-</html>
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+require_once("src/utils/debug.php");
+
+$action = htmlentities($_GET['action'] ?? '');
+
+
+
+include_once('templates/pages/list.php');
