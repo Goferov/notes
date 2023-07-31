aac9335dfd39ad2bd9ba2c83c1169e7a69b6e6d9
Goferov <marcingodfryd@gmail.com>
2023-07-31 22:53:19 +0200

    Refactoring the database.php

--- src/Database.php
+++ src/Database.php
@@ -14,18 +14,19 @@ class Database
 {
     public function __construct(array $config) {
         try {
-
-            if(empty($config['database']) || empty($config['host']) || empty($config['user']) || $config['password']) {
-                throw new ConfiguartionException('Database configuration error');
-            }
-
+            $this->validateConfig($config);
             $dsn = 'mysql:dbname='.$config['database'].';host='.$config['host'];
-//            $connection = new PDO($dsn,$config['user'],$config['password']);
-            $connection = new PDO('sss');
+            $connection = new PDO($dsn,$config['user'],$config['password']);
         }
         catch(PDOException $a) {
             throw new StorageException('Connection error');
         }
 
+    }
+
+    private function validateConfig(array $config): void {
+        if(empty($config['database']) || empty($config['host']) || empty($config['user']) || empty($config['password'])) {
+            throw new ConfiguartionException('Database configuration error');
+        }
     }
 }
