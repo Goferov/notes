3ae74b6b4de33a4df4cef1859f4ff3023f91b1cd
Goferov <marcingodfryd@gmail.com>
2023-08-02 22:20:21 +0200

    Refactor Controller class

--- src/Controller.php
+++ src/Controller.php
@@ -31,58 +31,47 @@ class Controller
         $this->view = new View();
     }
 
+    public function createAction() {
+        if($this->request->hasPost()) {
+            $this->db->createNote([
+                'title' => $this->request->postParam('title'),
+                'description' => $this->request->postParam('description'),
+            ]);
+            header('Location: /?msg=created');
+            exit();
+        }
+        $this->view->render('create');
+    }
 
-    public function run(): void {
-
-        $viewParams = [];
-        switch ($this->action()) {
-            case 'create':
-                $page = 'create';
-
-                if($this->request->hasPost()) {
-                    $this->db->createNote([
-                        'title' => $this->request->postParam('title'),
-                        'description' => $this->request->postParam('description'),
-                    ]);
-                    header('Location: /?msg=created');
-                    exit();
-                }
-
-                break;
-            case 'show':
-                $page = 'show';
-                $noteId = (int) $this->request->getParam('id');
-
-                if(!$noteId) {
-                    header('Location: /?error=missingNoteId');
-                    exit();
-                }
-
-                try {
-                    $note = $this->db->getNote($noteId);
-                }
-                catch (NotFoundException $e) {
-                    header('Location: /?error=noteNotFound');
-                    exit();
-                }
-
-                $viewParams = [
-                    'note' => $note,
-                ];
-
-                break;
-            default:
-                $page = 'list';
-
-                $viewParams = [
-                    'msg' => $this->request->getParam('msg'),
-                    'error' => $this->request->getParam('error'),
-                    'notes' => $this->db->getNotes(),
-                ];
-                break;
+    public function showAction() {
+        $noteId = (int) $this->request->getParam('id');
+        if(!$noteId) {
+            header('Location: /?error=missingNoteId');
+            exit();
         }
 
-        $this->view->render($page, $viewParams);
+        try {
+            $note = $this->db->getNote($noteId);
+        }
+        catch (NotFoundException $e) {
+            header('Location: /?error=noteNotFound');
+            exit();
+        }
+
+        $this->view->render('show', ['note' => $note,]);
+    }
+
+    public function listAction() {
+        $this->view->render('list', ['msg' => $this->request->getParam('msg'), 'error' => $this->request->getParam('error'), 'notes' => $this->db->getNotes()]);
+    }
+
+    public function run(): void {
+        $action = $this->action() . 'Action';
+
+        if(!method_exists($this, $action)) {
+            $action = self::DEFAULT_ACTION . 'Action';
+        }
+        $this->$action();
     }
     private function action(): string {
         return $this->request->getParam('action', self::DEFAULT_ACTION);
